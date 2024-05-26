6cb585414886152bd38bff6b3fa7dad2ebe23eb4
Dexter <dexter.t.jones@outlook.com>
2024-05-25 22:00:56 -0600

    Ready for main branch

--- Radix/src/Main.java
+++ Radix/src/Main.java
@@ -4,7 +4,7 @@ Author: Dexter Jones
 Implement radix sort algorithm.
 Analyze algorithm in Big-Oh notation and provide the appropriate analysis.
 Analyze the algorithm by documenting the steps taken when radix sort sorts
-the following array of Integer objects: 783 99 472 182 264 543 356 295 692 491 94
+the following array of String objects: joke book back dig desk word fish ward dish wit deed fast dog bend
  */
 import java.util.Arrays;
 public class Main {
