c703b039a80b86b957fe47aaaefb5937e768eb31
Dexter <dexter.t.jones@outlook.com>
2024-05-25 20:51:29 -0600

    Ready for main branch

--- Radix/src/Main.java
+++ Radix/src/Main.java
@@ -6,7 +6,13 @@ Analyze algorithm in Big-Oh notation and provide the appropriate analysis.
 Analyze the algorithm by documenting the steps taken when radix sort sorts
 the following array of Integer objects: 783 99 472 182 264 543 356 295 692 491 94
  */
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
+        int arr[] = {783, 99, 472, 182, 264, 543, 356, 295, 692, 491, 94};
+
+        System.out.println("Unordered array:\t" + Arrays.toString(arr));
+        RadixSort.radixIntSort(arr);
+        System.out.println("Sorted array:\t\t" + Arrays.toString(arr));
     }
 }
--- Radix/src/RadixSort.java
+++ Radix/src/RadixSort.java
@@ -1,2 +1,48 @@
+import java.util.Arrays;
+
 public class RadixSort {
+    static int getMax(int arr[], int n) {
+        int max = arr[0];
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+    static void countSort(int arr[], int n, int exp) {
+        int output[] = new int[n];
+        int count[] = new int[10];
+        Arrays.fill(count,0);
+
+        // count of occurrances stored in count[]
+        for (int i = 0; i < n; i++) {
+            count[(arr[i] / exp) % 10]++;
+        }
+        // change count[i] to contain actual position of digit in output[]
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        //Build the output array
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] /exp) % 10]--;
+        }
+
+        //copy output array to arr[] so it contains sorted numbers according to current digit
+        for (int i = 0; i < n; i++) {
+            arr[i] = output[i];
+        }
+
+    }
+    public static void radixIntSort(int[] arr) {
+        int l = arr.length;
+
+        int max = getMax(arr, l);
+
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countSort(arr, l, exp);
+        }
+    }
 }
